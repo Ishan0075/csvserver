790e5290d448eae744b7a6009da361d91bf1968d
ishan0075 <ishansharma1357@gmail.com>
2023-11-19 11:03:23 +0530

    first assignment

---
+++ solution/gencsv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+file=/test/csvserver/inputfile
+
+echo "$1, $2" > ${file}
+
+echo "$3, $4" >> ${file}
+
+echo "$5, $6" >> ${file}
+
+echo "$7, $8" >> ${file}
+
+echo "$9, ${10}" >> ${file}
+
+echo "${11}, ${12}" >> ${file}
+
+echo "${13}, ${14}" >> ${file}
